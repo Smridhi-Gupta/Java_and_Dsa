af74c8ec22c6658d60371165467cdafcc5f67122
Smridhi-Gupta <smridhi106@gmail.com>
2023-11-25 23:13:54 +0530

    init

--- src/College/Arrays.java
+++ src/College/Arrays.java
@@ -23,25 +23,4 @@ public class Arrays {
         return newarr;
     }
 
-    public static int[] delete(int n, int arr[], int pos) {
-        int positionToDelete = 2;
-    }
-    int[] arr = {1, 2, 3, 4, 5};
-    int positionToDelete = 2; // Change this to the position you want to delete
-
-    int[] newArr = new int[arr.length - 1];
-    int newIndex = 0;
-        for (int i = 0; i < arr.length; i++) {
-        if (i != positionToDelete) {
-            newArr[newIndex] = arr[i];
-            newIndex++;
-        }
-    }
-    arr = newArr;
-
-
-        System.out.print("Updated Array: ");
-        for (int num : arr) {
-        System.out.print(num +"");
-        }
 }
