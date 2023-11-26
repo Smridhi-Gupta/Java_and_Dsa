e5ea70cc236fd60b67b3478e91d5238dd5483777
Smridhi-Gupta <smridhi106@gmail.com>
2023-11-26 13:44:53 +0530

    init

--- src/College/Arrays.java
+++ src/College/Arrays.java
@@ -5,7 +5,11 @@ public class Arrays {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
         System.out.println(java.util.Arrays.toString(insert(arr.length, arr, 60,3)));
+        System.out.println(java.util.Arrays.toString(delete(arr, 2)));
+        System.out.println(java.util.Arrays.toString(update(arr, 35, 2)));
+        System.out.println(java.util.Arrays.toString(reverse(arr)));
     }
 
     public static int[] insert(int n, int arr[], int x, int pos) {
@@ -23,4 +27,39 @@ public class Arrays {
         return newarr;
     }
 
+    public static int[] delete(int arr[], int pos) {
+        int[] newarr = new int[arr.length-1];
+        int newIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i != pos) {
+                newarr[newIndex] = arr[i];
+                newIndex++;
+            }
+        }
+        arr = newarr;
+        return newarr;
+    }
+
+    public static int[] update(int arr[], int x, int pos) {
+        for (int i = 0; i < arr.length; i++) {
+            if(i == pos) {
+                arr[i] = x;
+            }
+        }
+        return arr;
+    }
+
+    public static int[] reverse(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int first = 0, last = arr.length-1;
+            while (first < last) {
+                int temp = arr[last];
+                arr[last] = arr[first];
+                arr[first] = temp;
+                first++;
+                last--;
+            }
+        }
+        return arr;
+    }
 }
