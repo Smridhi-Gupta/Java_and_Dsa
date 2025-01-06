17e513d8e91926df16150582828e8cfa93e01c29
Smridhi gupta <smridhi106@gmail.com>
2025-01-06 23:34:20 +0530

    init

--- Java_and_Dsa-master/src/linkedList/LinkedList.java
+++ Java_and_Dsa-master/src/linkedList/LinkedList.java
@@ -79,6 +79,27 @@ public class LinkedList {
         size--;
         return val;
     }
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        // prev : i = size - 2
+        Node prev = head;
+        for (int i = 0; i < size - 2 ; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data; // tail.data
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.print();
@@ -100,5 +121,9 @@ public class LinkedList {
 //        System.out.println(ll.size);
         ll.removeFirst();
         ll.print();
+
+        ll.removeLast();
+        ll.print();
+        System.out.println(ll.size);
     }
 }
