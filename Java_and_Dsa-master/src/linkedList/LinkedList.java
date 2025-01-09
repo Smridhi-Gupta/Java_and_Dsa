11161fb64328f639837f9363616aae07622c58b3
Smridhi gupta <smridhi106@gmail.com>
2025-01-09 13:00:31 +0530

    init

--- Java_and_Dsa-master/src/linkedList/LinkedList.java
+++ Java_and_Dsa-master/src/linkedList/LinkedList.java
@@ -262,6 +262,63 @@ public class LinkedList {
         prev.next = null;
     }
 
+    private Node getMid(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; // mid node
+    }
+
+    private Node merge(Node head1, Node head2) {
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL; // temp is ponting to merged linked list head
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1; // jhn choti value h wahn head krra h = head1
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2; // agr head2 chota h to ispe point krega temp
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+
+    public Node mergeSort(Node head) {
+        // ek value ya khali linked list
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // find mid
+        Node mid = getMid(head);
+
+        // left right half ke liye call mergesort
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+
+        // merge
+        return merge(newLeft, newRight);
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
 //        ll.print();
@@ -318,14 +375,25 @@ public class LinkedList {
 //        // 1->2->3->1
 //        System.out.println(isCycle());
 
-        head = new Node(1);
-        Node temp = new Node(2);
-        head.next = temp;
-        head.next.next = new Node(3);
-        head.next.next.next = temp;
-        // 1->2->3->2
-        System.out.println(isCycle());
-        removeCycle();
-        System.out.println(isCycle());
+//        head = new Node(1);
+//        Node temp = new Node(2);
+//        head.next = temp;
+//        head.next.next = new Node(3);
+//        head.next.next.next = temp;
+//        // 1->2->3->2
+//        System.out.println(isCycle());
+//        removeCycle();
+//        System.out.println(isCycle());
+
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        // 5->4->3->2->1
+
+        ll.print();
+        ll.head = ll.mergeSort(ll.head);
+        ll.print();
     }
 }
