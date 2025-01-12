6627aa57add4707a6aacc5730b29312425909b53
Smridhi gupta <smridhi106@gmail.com>
2025-01-12 14:14:14 +0530

    init

--- Java_and_Dsa-master/src/linkedList/DoublyLL.java
+++ Java_and_Dsa-master/src/linkedList/DoublyLL.java
@@ -17,14 +17,60 @@ public class DoublyLL {
     public static int size;
 
     // add first
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    // print
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data +"<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
 
     // add last
 
     // remove first
 
     // remove last
+    public int removeFirst() {
+        if (head == null) {
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;
+        }
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
 
     public static void main(String[] args) {
         DoublyLL dll = new DoublyLL();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.print();
+        System.out.println(dll.size);
+        dll.removeFirst();
+        dll.print();
+        System.out.println(dll.size);
     }
 }
