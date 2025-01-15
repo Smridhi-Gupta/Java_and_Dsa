480efd1f84a9583e4e5e115f6e47a85459c7278a
Smridhi gupta <smridhi106@gmail.com>
2025-01-16 00:01:28 +0530

    init

--- Java_and_Dsa-master/src/recursion/xPowern.java
+++ Java_and_Dsa-master/src/recursion/xPowern.java
@@ -13,8 +13,16 @@ public class xPowern {
             return 1;
         }
         int halfPower = optimisedPower(a, n/2);
+        int halfPowerSq = halfPower * halfPower;
+        if (n % 2 != 0) {
+            halfPowerSq = a * halfPowerSq;
+        }
+        return halfPowerSq;
     }
     public static void main(String[] args) {
         System.out.println(power(2, 10));
+        int a = 2;
+        int n = 10;
+        System.out.println(optimisedPower(a, n));
     }
 }
