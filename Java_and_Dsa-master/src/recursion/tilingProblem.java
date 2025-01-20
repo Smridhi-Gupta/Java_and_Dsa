3f46b07c719c4c3a3c5ef5bc3193e71b5139bc43
Smridhi gupta <smridhi106@gmail.com>
2025-01-20 23:18:35 +0530

    init

---
+++ Java_and_Dsa-master/src/recursion/tilingProblem.java
@@ -0,0 +1,22 @@
+package recursion;
+
+public class tilingProblem {
+    public static int tilingProb(int n) {
+        // 2 * n floor size
+        // base case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        // kaam
+        // vertical choice
+        int fnm1 = tilingProb(n - 1);
+        // horizontal choice
+        int fnm2 = tilingProb(n - 2);
+        int totalWays = fnm1 + fnm2;
+        return totalWays;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tilingProb(3));
+    }
+}
--- Java_and_Dsa-master/src/recursion/xPowern.java
+++ Java_and_Dsa-master/src/recursion/xPowern.java
@@ -12,13 +12,14 @@ public class xPowern {
         if (n == 0) {
             return 1;
         }
-        int halfPower = optimisedPower(a, n/2);
+        int halfPower = optimisedPower(a, n / 2);
         int halfPowerSq = halfPower * halfPower;
         if (n % 2 != 0) {
             halfPowerSq = a * halfPowerSq;
         }
         return halfPowerSq;
     }
+
     public static void main(String[] args) {
         System.out.println(power(2, 10));
         int a = 2;
