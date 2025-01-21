8fb2b0c1947b6e6e8c58b3996600fe9d7998274f
Smridhi gupta <smridhi106@gmail.com>
2025-01-21 16:12:27 +0530

    init

---
+++ Java_and_Dsa-master/src/stack/Stack2.java
@@ -0,0 +1,19 @@
+package stack;
+
+public class Stack2 {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static class stack {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
