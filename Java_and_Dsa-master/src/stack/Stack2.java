8dc000a3589a6ff2866c3b894ecc668f47e63e76
Smridhi gupta <smridhi106@gmail.com>
2025-01-25 22:17:59 +0530

    init

--- Java_and_Dsa-master/src/stack/Stack2.java
+++ Java_and_Dsa-master/src/stack/Stack2.java
@@ -10,7 +10,29 @@ public class Stack2 {
         }
     }
     static class stack {
-
+        static Node head = null;
+        public static boolean isEmpty() {
+            return head == null;
+        }
+        // push
+        public static void push(int data) {
+            Node newNode = new Node(data);
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+        // pop
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
     }
 
     public static void main(String[] args) {
