6ff164a7ba5fee119e2031cffd2412a784d12c2c
Smridhi gupta <smridhi106@gmail.com>
2025-01-27 23:45:23 +0530

    init

--- Java_and_Dsa-master/src/stack/validParenthesis.java
+++ Java_and_Dsa-master/src/stack/validParenthesis.java
@@ -1,7 +1,20 @@
 package stack;
 
+import java.util.Stack;
+
 public class validParenthesis {
     public static boolean isValid(String str) {
-        
+        Stack<Character> s = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            //opening
+            if (ch == '(' || ch == '{' || ch == '[') {
+                
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "({}) []"; // true
     }
 }
