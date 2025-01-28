a99968479e04b2d5eb10867940ce1afb3ffb08b5
Smridhi gupta <smridhi106@gmail.com>
2025-01-28 22:49:29 +0530

    init

---
+++ Java_and_Dsa-master/src/stack/maximumArea.java
@@ -0,0 +1,23 @@
+package stack;
+
+import java.util.Stack;
+
+public class maximumArea {
+    public static void maxArea(int arr[]) {
+        int maxArea = 0;
+        int nsr[] = new int[arr.length];
+        int nsl[] = new int[arr.length];
+        // next smaller right
+        Stack<Integer> s = new Stack<>();
+        for (int i = arr.length- 1; i >= 0 ; i--) {
+
+        }
+
+        // next smaller left
+
+        // current area
+    }
+    public static void main(String[] args) {
+        int arr[] = {2, 1, 5, 6, 2, 3}; // heights in histogram
+    }
+}
