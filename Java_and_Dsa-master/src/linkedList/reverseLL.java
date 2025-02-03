ed58b202d43303c81b5e1283dc5742a87d5c09b9
Smridhi gupta <smridhi106@gmail.com>
2025-02-03 23:18:12 +0530

    init

--- Java_and_Dsa-master/src/linkedList/reverseLL.java
+++ Java_and_Dsa-master/src/linkedList/reverseLL.java
@@ -1,19 +1,19 @@
-package linkedList;
-import java.util.*;
-
-public class reverseLL {
-    public void reverse() {
-        Node prev = null;
-        Node curr = tail = head;
-        Node next;
-        while (curr != null) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        } head = prev;
-    }
-    public static void main(String[] args) {
-        System.out.println(ll.reverse());
-    }
-}
+//package linkedList;
+//import java.util.*;
+//
+//public class reverseLL {
+//    public void reverse() {
+//        Node prev = null;
+//        Node curr = tail = head;
+//        Node next;
+//        while (curr != null) {
+//            next = curr.next;
+//            curr.next = prev;
+//            prev = curr;
+//            curr = next;
+//        } head = prev;
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(ll.reverse());
+//    }
+//}
--- Java_and_Dsa-master/src/stack/maximumArea.java
+++ Java_and_Dsa-master/src/stack/maximumArea.java
@@ -10,14 +10,46 @@ public class maximumArea {
         // next smaller right
         Stack<Integer> s = new Stack<>();
         for (int i = arr.length- 1; i >= 0 ; i--) {
-
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                // -1
+                nsr[i] = arr.length;
+            } else {
+                // top
+                nsr[i] = s.peek();
+            }
+            s.push(i);
         }
 
         // next smaller left
+        s = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                // -1
+                nsl[i] = arr.length;
+            } else {
+                // top
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
 
-        // current area
+        // current area : width = j-i-1 = nsr[i]-nsl[i]-1
+        for (int i = 0; i < arr.length; i++) {
+            int height = arr[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currArea = height * width;
+            maxArea = Math.max(currArea, maxArea);
+        }
+        System.out.println("max area in histogram =" + maxArea);
     }
     public static void main(String[] args) {
         int arr[] = {2, 1, 5, 6, 2, 3}; // heights in histogram
+        maxArea(arr);
     }
 }
