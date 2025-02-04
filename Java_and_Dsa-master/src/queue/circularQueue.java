aff0cf427fc48f869ff2bd6ad0f30577bf567b44
Smridhi gupta <smridhi106@gmail.com>
2025-02-04 14:01:13 +0530

    init

--- Java_and_Dsa-master/src/queue/circularQueue.java
+++ Java_and_Dsa-master/src/queue/circularQueue.java
@@ -43,12 +43,15 @@ public class circularQueue {
                 System.out.println("empty queue");
                 return -1;
             }
-            int front = arr[0];
-            for (int i = 0; i < rear; i++) {
-                arr[i] = arr[i + 1];
+            int result = arr[front];
+
+            // last element delete
+            if (rear == front) {
+                rear = front = -1;
+            } else {
+                front = (front + 1) % size;
             }
-            rear = rear - 1;
-            return front;
+            return result;
         }
 
         // peek
@@ -57,15 +60,19 @@ public class circularQueue {
                 System.out.println("empty queue");
                 return -1;
             }
-            return arr[0];
+            return arr[front];
         }
     }
 
     public static void main(String[] args) {
-        Queuee q = new Queuee(5);
+        Queuee q = new Queuee(3);
         q.add(1);
         q.add(2);
         q.add(3);
+        System.out.println(q.remove());
+        q.add(4);
+        System.out.println(q.remove());
+        q.add(5);
         while (!q.isEmpty()) {
             System.out.println(q.peek());
             q.remove();
