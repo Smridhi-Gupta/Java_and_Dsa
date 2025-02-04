fa6bd41148ccb64f7b59cb3d6902588b0dacc99c
Smridhi gupta <smridhi106@gmail.com>
2025-02-04 13:55:28 +0530

    init

---
+++ Java_and_Dsa-master/src/queue/circularQueue.java
@@ -0,0 +1,74 @@
+package queue;
+
+public class circularQueue {
+    static class Queuee {
+        static int arr[];
+        static int size;
+        static int rear;
+        static int front;
+
+        Queuee(int n) {
+            arr = new int[n];
+            size = n;
+            rear = -1;
+            front = -1;
+        }
+
+        public static boolean isEmpty() {
+            return rear == -1 && front == -1;
+        }
+
+        public static boolean isFull() {
+            return (rear + 1) % size == front;
+        }
+
+        // add
+        public static void add(int data) {
+            if (isFull()) {
+                System.out.println("queue is full");
+                return;
+            }
+
+            // adding frst element
+            if (front == -1) {
+                front = 0;
+            }
+            rear = (rear + 1) % size;
+            arr[rear] = data;
+        }
+
+        // remove
+        public static int remove() {
+            if(isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            int front = arr[0];
+            for (int i = 0; i < rear; i++) {
+                arr[i] = arr[i + 1];
+            }
+            rear = rear - 1;
+            return front;
+        }
+
+        // peek
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            return arr[0];
+        }
+    }
+
+    public static void main(String[] args) {
+        Queuee q = new Queuee(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
