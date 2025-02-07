ead664fa38d24666651cb7aaa8718fc3f8b01842
Smridhi gupta <smridhi106@gmail.com>
2025-02-08 00:07:25 +0530

    init

--- Java_and_Dsa-master/src/queue/QueueUsingTwoStacks.java
+++ Java_and_Dsa-master/src/queue/QueueUsingTwoStacks.java
@@ -8,6 +8,26 @@ public class QueueUsingTwoStacks {
         public static boolean isEmpty() {
             return s1.isEmpty();
         }
+
+        // add
+        public static void add(int data) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+            s1.push(data);
+            while (!s2.isEmpty()) {
+                s1.push(s2.pop());
+            }
+        }
+
+        // remove
+        public static int remove() {
+           if (isEmpty()) {
+               System.out.println("queue empty");
+               return -1;
+           }
+           return s1.pop();
+        }
     }
     public static void main(String[] args) {
 
