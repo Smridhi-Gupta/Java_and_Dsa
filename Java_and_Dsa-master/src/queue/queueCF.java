31c7220f2139fa5d4b14f17af29bfbb9f32ed379
Smridhi gupta <smridhi106@gmail.com>
2025-02-07 23:56:55 +0530

    init

--- Java_and_Dsa-master/src/queue/queueCF.java
+++ Java_and_Dsa-master/src/queue/queueCF.java
@@ -4,7 +4,8 @@ import java.util.Queue;
 
 public class queueCF {
     public static void main(String[] args) {
-        Queue<Integer> q = new LinkedList<>();
+//        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
         q.add(2);
         q.add(3);
