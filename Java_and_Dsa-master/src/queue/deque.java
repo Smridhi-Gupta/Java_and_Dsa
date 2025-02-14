9acf4ffa27e800c1d53b1bf6403af6739f4bfae1
Smridhi gupta <smridhi106@gmail.com>
2025-02-14 19:23:52 +0530

    init

---
+++ Java_and_Dsa-master/src/queue/deque.java
@@ -0,0 +1,13 @@
+package queue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class deque {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        System.out.println(deque);
+    }
+}
