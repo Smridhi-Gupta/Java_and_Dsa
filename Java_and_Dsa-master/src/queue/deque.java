4f8bf0566b1018911488b69d48cfd49c7186fc9d
Smridhi gupta <smridhi106@gmail.com>
2025-02-14 19:25:35 +0530

    init

--- Java_and_Dsa-master/src/queue/deque.java
+++ Java_and_Dsa-master/src/queue/deque.java
@@ -9,5 +9,12 @@ public class deque {
         deque.addFirst(1);
         deque.addFirst(2);
         System.out.println(deque);
+        deque.removeFirst();
+        System.out.println(deque);
+        deque.addLast(3);
+        deque.addLast(4);
+        System.out.println(deque);
+        deque.removeLast();
+        System.out.println(deque);
     }
 }
