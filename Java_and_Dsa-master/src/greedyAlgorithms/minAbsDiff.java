6da3dcb33ca40c3b7d1e794b5283aafa96c2a021
Smridhi gupta <smridhi106@gmail.com>
2025-02-16 02:00:30 +0530

    init

---
+++ Java_and_Dsa-master/src/greedyAlgorithms/minAbsDiff.java
@@ -0,0 +1,20 @@
+package greedyAlgorithms;
+import java.util.*;
+
+public class minAbsDiff {
+    public static void main(String[] args) {
+        int A[] = {4, 1, 8, 7};
+        int B[] = {2, 3, 6, 5};
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int minDiff = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            minDiff += Math.abs(A[i] - B[i]);
+        }
+
+        System.out.println("minimum absolute difference of pairs = " +minDiff);
+    }
+}
