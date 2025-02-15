e2527d087834efb5ac24e213b8574e3b0fb685c1
Smridhi gupta <smridhi106@gmail.com>
2025-02-16 01:26:51 +0530

    init

--- Java_and_Dsa-master/src/greedyAlgorithms/fractionalKnapsack.java
+++ Java_and_Dsa-master/src/greedyAlgorithms/fractionalKnapsack.java
@@ -4,6 +4,14 @@ import java.util.*;
 public class fractionalKnapsack {
     public static void main(String[] args) {
         int val[] = {60, 100, 120};
+        int weight[] = {10, 20, 30};
+        int w = 50;
+        double ratio[][] = new double[val.length][2];
 
+        // 0th column => idx , 1st column => ratio
+        for (int i = 0; i < val.length; i++) {
+            ratio[i][0] = i;
+            ratio[]
+        }
     }
 }
