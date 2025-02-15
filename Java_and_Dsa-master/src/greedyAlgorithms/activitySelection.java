4aac1395e86955ea6fa49a98ad5b4c29bddb7816
Smridhi gupta <smridhi106@gmail.com>
2025-02-15 23:24:59 +0530

    init

--- Java_and_Dsa-master/src/greedyAlgorithms/activitySelection.java
+++ Java_and_Dsa-master/src/greedyAlgorithms/activitySelection.java
@@ -2,7 +2,11 @@ package greedyAlgorithms;
 
 public class activitySelection {
     public static void main(String[] args) {
-        int start[] = {1, }
+        int start[] = {1, 3, 0, 5, 8, 5};
+        int end[] = {2, 4, 6, 7, 9, 9};
+
+        // sorting
+        int activities[][] = new int[start.length][3];
     }
 
 }
