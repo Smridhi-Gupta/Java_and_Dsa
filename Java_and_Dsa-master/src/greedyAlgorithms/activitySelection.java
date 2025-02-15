7a79c29ccf064b14d5444eff87f081419781de72
Smridhi gupta <smridhi106@gmail.com>
2025-02-15 23:32:56 +0530

    init

--- Java_and_Dsa-master/src/greedyAlgorithms/activitySelection.java
+++ Java_and_Dsa-master/src/greedyAlgorithms/activitySelection.java
@@ -28,6 +28,19 @@ public class activitySelection {
         // 1st activity
         maxAct = 1;
         ans.add(activities[0][0]);
+        int lastEnd = activities[0][2];
+
+        for (int i = 1; i < end.length; i++) {
+            if (activities[i][1] >= lastEnd) {
+
+                // activity select
+                maxAct++;
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
+            }
+        }
+
+        System.out.println();
     }
 
 }
