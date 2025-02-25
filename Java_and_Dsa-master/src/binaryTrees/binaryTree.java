aacacc0aa5c73b69ec077a80ae1f113a91510831
Smridhi gupta <smridhi106@gmail.com>
2025-02-25 16:24:18 +0530

    init

--- Java_and_Dsa-master/src/binaryTrees/binaryTree.java
+++ Java_and_Dsa-master/src/binaryTrees/binaryTree.java
@@ -28,7 +28,7 @@ public class binaryTree {
     }
 
     public static void main(String[] args) {
-        int nodes[] = {1, 2, 4, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BT tree = new BT();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
