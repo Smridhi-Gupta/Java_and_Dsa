961973d8def33c979e8ae313e3ce8760a78b62e5
Smridhi gupta <smridhi106@gmail.com>
2025-02-25 16:00:32 +0530

    init

---
+++ Java_and_Dsa-master/src/binaryTrees/binaryTree.java
@@ -0,0 +1,32 @@
+package binaryTrees;
+
+public class binaryTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BT {
+        static int idx = -1;
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
