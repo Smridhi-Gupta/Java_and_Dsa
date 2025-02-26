c4b7ce4044f8c3d4454e22f957b8702760f168ca
Smridhi gupta <smridhi106@gmail.com>
2025-02-26 12:59:54 +0530

    init

--- Java_and_Dsa-master/src/binaryTrees/heightOfTree.java
+++ Java_and_Dsa-master/src/binaryTrees/heightOfTree.java
@@ -30,6 +30,15 @@ public class heightOfTree {
         return leftCount + rightCount + 1;
     }
 
+    public static int sum(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftSum = sum(root.left);
+        int rightSum = sum(root.right);
+        return leftSum + rightSum + root.data;
+    }
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
@@ -40,5 +49,6 @@ public class heightOfTree {
         root.right.right = new Node(7);
         System.out.println(height(root));
         System.out.println(count(root));
+        System.out.println(sum(root));
     }
 }
