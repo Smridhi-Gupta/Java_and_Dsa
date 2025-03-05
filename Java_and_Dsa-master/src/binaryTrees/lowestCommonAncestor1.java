3b947001b224faa1f1811ff775febf015a43b459
Smridhi gupta <smridhi106@gmail.com>
2025-03-05 22:53:55 +0530

    init

--- Java_and_Dsa-master/src/binaryTrees/lowestCommonAncestor1.java
+++ Java_and_Dsa-master/src/binaryTrees/lowestCommonAncestor1.java
@@ -18,15 +18,20 @@ public class lowestCommonAncestor1 {
         if (root == null) {
             return false;
         }
+
         path.add(root);
+
         if (root.data == n) {
             return true;
         }
+
         boolean foundLeft = getPath(root.left, n, path);
         boolean foundRight = getPath(root.right, n, path);
+
         if (foundLeft || foundRight) {
             return true;
         }
+
         path.remove(path.size() - 1);
         return false;
     }
