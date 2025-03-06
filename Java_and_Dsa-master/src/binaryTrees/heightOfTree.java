ddd3248926d9e19716984d9404046d60041cbebb
Smridhi gupta <smridhi106@gmail.com>
2025-03-06 22:52:05 +0530

    init

--- Java_and_Dsa-master/src/binaryTrees/heightOfTree.java
+++ Java_and_Dsa-master/src/binaryTrees/heightOfTree.java
@@ -21,49 +21,6 @@ public class heightOfTree {
         return Math.max(lh, rh) + 1;
     }
 
-//    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
-//        if (root == null) {
-//            return false;
-//        }
-//
-//        path.add(root);
-//
-//        if (root.data == n) {
-//            return true;
-//        }
-//
-//        boolean foundLeft = getPath(root.left, n, path);
-//        boolean foundRight = getPath(root.right, n, path);
-//
-//        if (foundLeft || foundRight) {
-//            return true;
-//        }
-//
-//        path.remove(path.size() - 1);
-//        return false;
-//    }
-
-//    public static Node lca(Node root, int n1, int n2) {
-//        ArrayList<Node> path1 = new ArrayList<>();
-//        ArrayList<Node> path2 = new ArrayList<>();
-//
-//        // root se node tak ka path store in path1/path2
-//        getPath(root, n1, path1);
-//        getPath(root, n2, path2);
-//
-//        // last common ancestor
-//        int i = 0;
-//        for (; i < path1.size() && path2.size(); i++) {
-//            if (path1.get(i) != path1.get(i)) {
-//                break;
-//            }
-//        }
-//
-//        // last equal node -> i-1th value
-//        Node lca = path1.get(i - 1);
-//        return lca;
-//    }
-
     public static void main(String[] args) {
 //             1
 //           /   \
@@ -80,8 +37,6 @@ public class heightOfTree {
 
         System.out.println("height of a tree : " + height(root));
 
-        int n1 = 4, n2 = 5;
-//        System.out.println(lca(root, n1, n2).data);
 //          2
 //         / \
 //        4   5
