4855cd3b43b970e270a7436eea8cf7eab089e59f
Smridhi gupta <smridhi106@gmail.com>
2025-03-06 22:57:18 +0530

    init

--- Java_and_Dsa-master/src/binaryTrees/minimumDistance.java
+++ Java_and_Dsa-master/src/binaryTrees/minimumDistance.java
@@ -12,6 +12,31 @@ public class minimumDistance {
         }
     }
 
+    public static Node lca2(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+        // left LCA = val    right LCA = null
+        if (rightLca == null) {
+            return leftLca;
+        }
+
+        if (leftLca == null) {
+            return rightLca;
+        }
+
+        return root;
+    }
+
+    public static int minDist(Node root, int n1, int n2) {
+        Node lca = lca2(root, n1, n2);
+
+    }
+
     public static void main(String[] args) {
 //             1
 //           /   \
