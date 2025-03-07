d17aa843cb042854c966cddbfda2768381d6deff
Smridhi gupta <smridhi106@gmail.com>
2025-03-07 23:21:00 +0530

    init

--- Java_and_Dsa-master/src/binaryTrees/kthAncestor.java
+++ Java_and_Dsa-master/src/binaryTrees/kthAncestor.java
@@ -12,21 +12,26 @@ public class kthAncestor {
         }
     }
 
-    public static int KAnncestor(Node root, int n, int k) {
+    public static int KAncestor(Node root, int n, int k) {
         if (root == null)
             return -1;
 
         if (root.data == n)
             return 0;
 
-        int leftDist = KAnncestor(root.left, n, k);
-        int rightDist = KAnncestor(root.right, n, k);
+        int leftDist = KAncestor(root.left, n, k);
+        int rightDist = KAncestor(root.right, n, k);
 
 
         if (leftDist == -1 && rightDist == -1)
             return -1;
 
-        int max =
+        int max = Math.max(leftDist, rightDist);
+
+        if (max + 1 == k)
+            System.out.println(root.data);
+
+        return max + 1;
     }
 
     public static void main(String[] args) {
@@ -35,12 +40,15 @@ public class kthAncestor {
 //          2     3
 //         / \   / \
 //        4   5 6   7
-        minimumDistance.Node root = new minimumDistance.Node(1);
-        root.left = new minimumDistance.Node(2);
-        root.right = new minimumDistance.Node(3);
-        root.left.left = new minimumDistance.Node(4);
-        root.left.right = new minimumDistance.Node(5);
-        root.right.left = new minimumDistance.Node(6);
-        root.right.right = new minimumDistance.Node(7);
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int n = 5, k = 2;
+        KAncestor(root, n, k);
     }
 }
