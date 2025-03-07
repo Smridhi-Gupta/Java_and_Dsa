9c7732fc71dec237880b3e0653339f511a0a53cf
Smridhi gupta <smridhi106@gmail.com>
2025-03-07 23:57:48 +0530

    init

--- Java_and_Dsa-master/src/binaryTrees/transformToSumTree.java
+++ Java_and_Dsa-master/src/binaryTrees/transformToSumTree.java
@@ -13,12 +13,29 @@ public class transformToSumTree {
     }
 
     public static int transform(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
         int leftChild = transform(root.left);
         int rightChild = transform(root.right);
 
         int data = root.data;
-        root.data = root.left.data + leftChild + root.right.data + rightChild;
+
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+
+        root.data = newLeft + leftChild + newRight + rightChild;
         return data;
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
     }
 
     public static void main(String[] args) {
@@ -34,5 +51,8 @@ public class transformToSumTree {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
+
+        transform(root);
+        preorder(root);
     }
 }
