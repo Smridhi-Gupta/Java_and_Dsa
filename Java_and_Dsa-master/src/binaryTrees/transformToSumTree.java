1805cde3d4fd0a210f7c20777d4225610b7c50f6
Smridhi gupta <smridhi106@gmail.com>
2025-03-07 23:52:39 +0530

    init

--- Java_and_Dsa-master/src/binaryTrees/transformToSumTree.java
+++ Java_and_Dsa-master/src/binaryTrees/transformToSumTree.java
@@ -3,7 +3,7 @@ package binaryTrees;
 public class transformToSumTree {
     static class Node {
         int data;
-        lowestCommonAncestor2.Node left, right;
+        Node left, right;
 
         public Node(int data) {
             this.data = data;
@@ -12,18 +12,27 @@ public class transformToSumTree {
         }
     }
 
+    public static int transform(Node root) {
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+
+        int data = root.data;
+        root.data = root.left.data + leftChild + root.right.data + rightChild;
+        return data;
+    }
+
     public static void main(String[] args) {
-        //             1
+//             1
 //           /   \
 //          2     3
 //         / \   / \
 //        4   5 6   7
-        lowestCommonAncestor2.Node root = new lowestCommonAncestor2.Node(1);
-        root.left = new lowestCommonAncestor2.Node(2);
-        root.right = new lowestCommonAncestor2.Node(3);
-        root.left.left = new lowestCommonAncestor2.Node(4);
-        root.left.right = new lowestCommonAncestor2.Node(5);
-        root.right.left = new lowestCommonAncestor2.Node(6);
-        root.right.right = new lowestCommonAncestor2.Node(7);
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
     }
 }
