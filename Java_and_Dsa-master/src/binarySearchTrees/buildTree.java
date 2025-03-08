87c0d90bec7aef487869443a42b4e689d43755c4
Smridhi gupta <smridhi106@gmail.com>
2025-03-08 23:25:41 +0530

    init

---
+++ Java_and_Dsa-master/src/binarySearchTrees/buildTree.java
@@ -0,0 +1,24 @@
+package binarySearchTrees;
+
+import binaryTrees.transformToSumTree;
+
+public class buildTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        if
+    }
+}
