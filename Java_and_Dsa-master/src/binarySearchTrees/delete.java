e817bf6d1af9149efbf39bf66c54ba69a6cb2020
Smridhi gupta <smridhi106@gmail.com>
2025-03-09 23:52:39 +0530

    init

--- Java_and_Dsa-master/src/binarySearchTrees/delete.java
+++ Java_and_Dsa-master/src/binarySearchTrees/delete.java
@@ -2,7 +2,7 @@ package binarySearchTrees;
 
 public class delete {
     public static void main(String[] args) {
-        int values[] = {5, 1, 3, 4, 2, 7};
+        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14 };
         search.Node root = null;
         for (int i = 0; i < values.length; i++) {
             root = insert(root, values[i]);
