05ff41c90b9fb944120d3e92dc2e3e3d0f5b6692
Smridhi gupta <smridhi106@gmail.com>
2025-03-25 16:41:58 +0530

    init

--- Java_and_Dsa-master/src/binarySearchTrees/delete.java
+++ Java_and_Dsa-master/src/binarySearchTrees/delete.java
@@ -3,8 +3,8 @@ package binarySearchTrees;
 public class delete {
     static class Node {
         int data;
-        search.Node left;
-        search.Node right;
+        Node left;
+        Node right;
 
         Node(int data) {
             this.data = data;
@@ -14,6 +14,23 @@ public class delete {
     public static Node delete(Node root, int val) {
         if (root.data < val) {
             root.right = delete(root.right, val);
+        } else if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else {
+            // case 1 - leaf node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            // case 2 - single child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // case 3 - both children
+
         }
     }
 }
