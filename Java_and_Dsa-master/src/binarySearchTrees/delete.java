0b85fd1c1c1d3556f049f0fed912a11ed4ffa3a3
Smridhi gupta <smridhi106@gmail.com>
2025-03-27 23:44:41 +0530

    init

--- Java_and_Dsa-master/src/binarySearchTrees/delete.java
+++ Java_and_Dsa-master/src/binarySearchTrees/delete.java
@@ -11,6 +11,21 @@ public class delete {
         }
     }
 
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        if (root.data > val) {
+            // left subtree
+            root.left = insert(root.left, val);
+        } else { // right subtree
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
     public static void inorder(Node root) {
         if (root == null) {
             return;
