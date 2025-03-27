61bfe6f253a85e284f46220363a344190428732e
Smridhi gupta <smridhi106@gmail.com>
2025-03-27 23:52:41 +0530

    init

--- Java_and_Dsa-master/src/binarySearchTrees/printInRange.java
+++ Java_and_Dsa-master/src/binarySearchTrees/printInRange.java
@@ -11,6 +11,30 @@ public class printInRange {
         }
     }
 
+    public static delete.Node insert(delete.Node root, int val) {
+        if (root == null) {
+            root = new delete.Node(val);
+            return root;
+        }
+
+        if (root.data > val) {
+            // left subtree
+            root.left = insert(root.left, val);
+        } else { // right subtree
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void inorder(delete.Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+
     public static void printRange(Node root, int k1, int k2) {
         if (root == null) {
             return;
@@ -27,7 +51,19 @@ public class printInRange {
         }
 
         else {
-
+            printRange(root.right, k1, k2);
         }
+    }
+
+    public static void main(String[] args) {
+        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+        delete.Node root = null;
+
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        inorder(root);
+        System.out.println();
     }
 }
