145ae03e8767bdf111d3a70e39523475fc2aa673
Smridhi gupta <smridhi106@gmail.com>
2025-04-01 21:16:54 +0530

    init

--- Java_and_Dsa-master/src/binarySearchTrees/validBST.java
+++ Java_and_Dsa-master/src/binarySearchTrees/validBST.java
@@ -10,4 +10,28 @@ public class validBST {
             this.data = data;
         }
     }
+
+    public static printRootToLeaf.Node insert(printRootToLeaf.Node root, int val) {
+        if (root == null) {
+            root = new printRootToLeaf.Node(val);
+            return root;
+        }
+
+        if (root.data > val) {
+            // left subtree
+            root.left = insert(root.left, val);
+        } else { // right subtree
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void inorder(printRootToLeaf.Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
 }
