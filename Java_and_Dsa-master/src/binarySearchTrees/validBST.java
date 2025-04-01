baee97ea51cec8f2a104d61b0c5285d95f813358
Smridhi gupta <smridhi106@gmail.com>
2025-04-01 21:16:14 +0530

    init

---
+++ Java_and_Dsa-master/src/binarySearchTrees/validBST.java
@@ -0,0 +1,13 @@
+package binarySearchTrees;
+
+public class validBST {
+    static class Node {
+        int data;
+        printRootToLeaf.Node left;
+        printRootToLeaf.Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+}
