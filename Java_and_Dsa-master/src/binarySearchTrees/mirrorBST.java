47e4a78159ce330747aff7eb6968334bc1b29cf2
Smridhi gupta <smridhi106@gmail.com>
2025-04-03 22:57:39 +0530

    init

--- Java_and_Dsa-master/src/binarySearchTrees/mirrorBST.java
+++ Java_and_Dsa-master/src/binarySearchTrees/mirrorBST.java
@@ -3,11 +3,34 @@ package binarySearchTrees;
 public class mirrorBST {
     static class Node {
         int data;
-        validBST.Node left;
-        validBST.Node right;
+        Node left;
+        Node right;
 
-        Node(int data) {
+        public Node(int data) {
             this.data = data;
+            this.left = this.right = null;
         }
+    }
+
+    public static void main(String[] args) {
+//            8
+//           / \
+//          5   10
+//         / \   \
+//        3   6   11
+
+        Node root = new Node(8);
+        root.left = new Node(5);
+        root.right = new Node(10);
+        root.left.left = new Node(3);
+        root.left.right = new Node(6);
+        root.right.right = new Node(11);
+
+//      mirror BST should be like this
+//            8
+//           / \
+//         10   5
+//         /   / \
+//        11  6   3
     }
 }
