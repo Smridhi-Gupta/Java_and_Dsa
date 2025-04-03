c70a38039c0b29f0e142b0a296490cd4c1b1e7b6
Smridhi gupta <smridhi106@gmail.com>
2025-04-03 22:50:02 +0530

    init

---
+++ Java_and_Dsa-master/src/binarySearchTrees/mirrorBST.java
@@ -0,0 +1,13 @@
+package binarySearchTrees;
+
+public class mirrorBST {
+    static class Node {
+        int data;
+        validBST.Node left;
+        validBST.Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+}
