04bf25afa1bd0c789e151890a12074ca34053613
Smridhi gupta <smridhi106@gmail.com>
2025-04-04 23:07:47 +0530

    init

--- Java_and_Dsa-master/src/binarySearchTrees/BSTtoBalancedBST.java
+++ Java_and_Dsa-master/src/binarySearchTrees/BSTtoBalancedBST.java
@@ -3,8 +3,8 @@ package binarySearchTrees;
 public class BSTtoBalancedBST {
     static class Node {
         int data;
-        sortedArrayToBalancedBST.Node left;
-        sortedArrayToBalancedBST.Node right;
+        Node left;
+        Node right;
 
         public Node(int data) {
             this.data = data;
@@ -12,12 +12,40 @@ public class BSTtoBalancedBST {
         }
     }
 
-    public static void preorder(sortedArrayToBalancedBST.Node root) {
+    public static void preorder(Node root) {
         if (root == null) {
             return;
         }
         System.out.print(root.data+" ");
         preorder(root.left);
         preorder(root.right);
+    }
+
+    public static void main(String[] args) {
+//              8
+//             / \
+//            6   10
+//           /      \
+//          5        11
+//         /           \
+//        3             12
+//        given BST
+
+        Node root = new Node(8);
+        root.left = new Node(6);
+        root.left.left = new Node(5);
+        root.left.left.left = new Node(3);
+
+        root.right = new Node(10);
+        root.right.right = new Node(11);
+        root.right.right.right = new Node(12);
+
+
+//            8
+//           / \
+//          5   11
+//         /\   / \
+//        3  6 10  12
+//        expected BST
     }
 }
