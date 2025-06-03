deff7fe3078d1db9bbe871b073868d9efed48daa
Smridhi gupta <smridhi106@gmail.com>
2025-06-04 00:13:50 +0530

    init

---
+++ Java_and_Dsa-master/src/series.java
@@ -0,0 +1,13 @@
+public class series {
+    public static void main(String[] args) {
+        int num = 3;
+        int step = 2;
+
+        for (int i = 0; i < 4; i++) {
+            System.out.print(num+" ");
+            num = num + step;
+            step++;
+        }
+
+    }
+}
